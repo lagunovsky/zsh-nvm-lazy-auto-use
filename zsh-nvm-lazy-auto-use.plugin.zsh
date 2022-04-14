8522d037c220a1a0a28de04d5c7a9037b7c3e0fb
Ivan Lagunovsky <ivan.lagunovsky@gmail.com>
2022-04-14 18:15:02 +0200

    fixed return to previous version

--- zsh-nvm-lazy-auto-use.plugin.zsh
+++ zsh-nvm-lazy-auto-use.plugin.zsh
@@ -2,23 +2,37 @@ ZSH_NVM_DIR=${0:a:h}
 
 [[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
 
-_zsh_nvm_has() {
-  type "$1" > /dev/null 2>&1
+_zsh_nvm_find_nvmrc () {
+  local path_="${PWD}"
+
+	while [ "${path_}" != "" ] && [ ! -f "${path_}/.nvmrc" ]
+	do
+		path_=${path_%/*}
+	done
+
+	if [ -e "${path_}/.nvmrc" ]
+	then
+		echo "${path_}/.nvmrc"
+	fi
 }
 
 autoload -U add-zsh-hook
 _zsh_nvm_lazy_auto_use() {
-  local nvmrc_path="$(pwd)/.nvmrc"
-
+  local nvmrc_path="$(_zsh_nvm_find_nvmrc)"
   if [[ -f "$nvmrc_path" ]]; then
     local nvmrc_node_version="$(nvm version $(cat "$nvmrc_path"))"
-
-    if [[ "$nvmrc_node_version" != "N/A" ]]; then
+    local node_version="$(node -v)"
+    if [[ "$nvmrc_node_version" != "N/A" && "$node_version" != "$nvmrc_node_version" ]]; then
      nvm use && export NVM_LAZY_AUTO_USE_ACTIVE=true
     fi
   else
-    _zsh_nvm_has nvm_find_nvmrc || return
-    nvm use default && export NVM_AUTO_USE_ACTIVE=false
+    if [[ "$NVM_LAZY_AUTO_USE_ACTIVE" == true ]]; then
+      local nvm_default_node_version="$(nvm version default)"
+      local node_version="$(node -v)"
+      if [[ "$node_version" != "$nvm_default_node_version" ]]; then
+        nvm use default && export NVM_LAZY_AUTO_USE_ACTIVE=false
+      fi
+    fi
   fi
 }
 
